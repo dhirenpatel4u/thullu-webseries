bd82425b94457cc171750fe6f7a36f509bdedfaa
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2026-07-19 00:40:23 +0530

    Update cast.js

--- src/utils/cast.js
+++ src/utils/cast.js
@@ -1,13 +1,36 @@
 export const initializeCast = (setReady) => {
-    window.__onGCastApiAvailable = (available) => {
-        if (!available) return;
 
-        cast.framework.CastContext.getInstance().setOptions({
-            receiverApplicationId:
-                chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
-            autoJoinPolicy:
-                chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
-        });
+    window.__onGCastApiAvailable = (
+        available
+    ) => {
+
+        if (
+            !available ||
+            !window.cast ||
+            !window.cast.framework ||
+            !window.chrome ||
+            !window.chrome.cast
+        ) {
+            return;
+        }
+
+        window.cast.framework
+            .CastContext
+            .getInstance()
+            .setOptions({
+                receiverApplicationId:
+                    window.chrome.cast.media
+                        .DEFAULT_MEDIA_RECEIVER_APP_ID,
+
+                autoJoinPolicy:
+                    window.chrome.cast
+                        .AutoJoinPolicy
+                        .ORIGIN_SCOPED
+            });
+
+        console.log(
+            "Chromecast initialized"
+        );
 
         setReady(true);
     };
